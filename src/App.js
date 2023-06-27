331573fb217340d45f596bb1109b21f48975f15c
Mani199810 <113034213+Mani199810@users.noreply.github.com>
2023-06-27 19:29:46 +0000

    'done'

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import "./App.css"
+import Screen2 from './Screen2';
+import Screen1 from './Screen1'
+import {BrowserRouter,Routes,Route } from 'react-router-dom'
+
+
+export default function App() {
+    
+  
+
+    return (
+        <>
+        <div className='home'>
+
+        <div className='child'>
+            <a href="/screen1"><button   >Screen1 </button> </a> 
+            <a  href="/screen2"> <button> Screen2</button> </a>
+        </div>
+
+        </div>
+        
+        <BrowserRouter>
+        <Routes>
+          <Route element={<Screen1/>} path="/screen1"></Route>
+          <Route element={<Screen2/>} path="/screen2"></Route>
+        </Routes>
+        
+        </BrowserRouter>
+        
+        
+
+        
+        </>
+    );
+}
+        
